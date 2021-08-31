8ef4f41f9ad4d759d00ce966d0e2cadd8d26b0e5
Lukasz Dobrowolski <l00k+github@protonmail.com>
2021-08-31 23:09:25 +0200

    init

---
+++ Component/UI/FilterField/FilterRange.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-field
+        label="Range"
+        label-position="on-border"
+    >
+        <b-input
+            v-model.lazy="rawValue.gte"
+            placeholder="Min"
+            @change.native="onRawValueChange"
+        />
+        <b-input
+            v-model.lazy="rawValue.lte"
+            placeholder="Max"
+            @change.native="onRawValueChange"
+        />
+    </b-field>
+</template>
+
+<script lang="ts">
+import UiFilterBase from '#/Core/Component/UI/FilterField/FilterBase.vue';
+import { Component } from '@/core/Vue/Annotations';
+
+
+@Component()
+export default class UiFilterRange
+    extends UiFilterBase
+{
+
+    public rawValue = {
+        gte: '',
+        lte: '',
+    };
+
+}
+</script>
---
+++ Component/UI/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-modal
+        :active.sync="visible"
+        v-bind="$attrs"
+        v-on="$listeners"
+    >
+        <slot>aaaa</slot>
+    </b-modal>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+
+import BaseComponent from '@/core/Vue/BaseComponent.vue';
+
+
+@Component
+export default class UiModal
+    extends BaseComponent
+{
+
+    public visible : boolean = false;
+
+    public show()
+    {
+        this.visible = true;
+    }
+
+    public hide()
+    {
+        this.visible = false;
+    }
+
+}
+</script>
